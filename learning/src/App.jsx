bbae733a35de839b7a4a3e01bfbb6ed7d48f48e0
murtaza-khan-git <mk8459215@gmail.com>
2025-03-12 12:13:11 +0530

    first commit

---
+++ learning/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+import Header from './component/Header'
+import HeroSection from './Component/HeroSection'
+import Section from './Component/Section'
+import TrustedBy from './Component/TrustedBy'
+import Employe from './Component/Employe'
+import Talior from './Component/Talior'
+import Customer from './Component/Customer'
+import Recantjobs from './Component/Recantjobs'
+import Footers from './Component/Footers'
+import Hero from './Component/hero'
+import JobCards from './Component/JobCards'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Section/>
+    <TrustedBy/>
+    <HeroSection/>
+    <Employe/>
+    <Talior/>
+    <Customer/>
+    <Recantjobs/>
+<Footers/>
+    
+    </>
+  )
+}
+
+export default App
